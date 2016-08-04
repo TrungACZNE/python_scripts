297ad41b9f96438ec13bdf6db691a1ec529605c9
Ngo The Trung <ngo.the.trung.aczne@gmail.com>
2016-08-05 00:43:17 +0800

    Update

--- neovim_tools/vim-shell.py
+++ neovim_tools/vim-shell.py
@@ -1,12 +1,77 @@
 #!/usr/bin/env python3
+"""
+Helpers for the neovim python client
+Run this file to start an interactive python shell that can interact with
+a running neovim process through the $NVIM_LISTEN_ADDRESS socket
+See reattach() for the exposed variables
+"""
 import code
 import json
+import os
 import yaml
 from neovim import attach
 
 
+identity = lambda x: x
+
+
+def throw(*args, **kwargs):
+    raise "Inappropriate action"
+
+
+FORMAT = {
+    "text": {
+        "encoder": identity,
+        "decoder": identity
+    },
+    "json": {
+        "encoder": lambda text: json.dumps(text, sort_keys=True, indent=4),
+        "decoder": json.loads
+    },
+    "yaml": {
+        "encoder": yaml.dump,
+        "decoder": yaml.load,
+    },
+    "eval": {
+        "encoder": eval,
+        "decoder": throw,
+    },
+    "exec": {
+        "encoder": lambda blob: exec(blob, globals()),
+        "decoder": throw,
+    }
+}
+
+
+def encode(data, encoder):
+    if type(encoder) == str:
+        if encoder in FORMAT:
+            return FORMAT[encoder]["encoder"](data)
+        else:
+            raise "Unknown encoder " + encoder
+    else:
+        # custom encoder function
+        return encoder(data)
+
+
+def decode(data, decoder):
+    if type(decoder) == str:
+        if decoder in FORMAT:
+            return FORMAT[decoder]["decoder"](data)
+        else:
+            raise "Unknown decoder " + decoder
+    else:
+        # custom decoder function
+        return decoder(data)
+
+
 def reattach():
-    nvim = attach('socket', path='/tmp/nvimsocket')
+    """
+    Attach to a running neovim process
+    Exposes some global variables for ease of use
+    Call again if detached (neovim process restarts)
+    """
+    nvim = attach('socket', path=os.environ.get("NVIM_LISTEN_ADDRESS", '/tmp/nvimsocket'))
     environment=globals()
     environment["nvim"] = nvim
     environment["buffers"] = nvim.buffers
@@ -26,7 +91,10 @@ def get_selected_pos_as_indices():
     return start_line - 1, start_col - 1, end_line - 1, end_col - 1
 
 
-def get_selected(decoder="text"):
+def selected(decoder="text"):
+    """
+    Return the selected text, decoded using decoder
+    """
     start_line, start_col, end_line, end_col = get_selected_pos_as_indices()
 
     if start_line == end_line:
@@ -38,25 +106,15 @@ def get_selected(decoder="text"):
             + [current.buffer[end_line][:end_col + 1]]
         )
 
-    if decoder == "json":
-        return json.loads(blob)
-    elif decoder == "yaml":
-        return yaml.load(blob)
-    elif decoder == "text":
-        return blob
-    elif decoder == "eval":
-        return eval(blob)
-    elif decoder == "exec":
-        return exec(blob, globals())
-    else:
-        raise "Unknown decoder"
+    return decode(blob, decoder)
 
 
-def replace_selected(blob):
-    if type(blob) is list:
-        lines = blob
-    else:
-        lines = blob.splitlines()
+def transform(data, encoder="text"):
+    """
+    Transform selected text into blob (multiple line string or array of lines)
+    """
+    blob = encode(data, encoder)
+    lines = blob.splitlines()
 
     if len(lines) == 0:
         return
@@ -81,8 +139,15 @@ def replace_selected(blob):
     else:
         current.buffer[start_line] = prefix + lines[0]
         for i, line in enumerate(lines[1:-1]):
-            current.buffer[start_line + i] = lines[i]
+            current.buffer[start_line + i + 1] = lines[i + 1]
         current.buffer[end_line] = lines[-1] + suffix
+
+
+def transform_lines(fun):
+    """
+    Transform selected text, line by line, using fun
+    """
+    transform(list(map(fun, selected().splitlines())))
 
 
 reattach()
